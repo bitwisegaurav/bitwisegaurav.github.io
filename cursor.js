a102bcd6a95162f3f07e39df6abb8b7a3b5321b2
Gaurav Mishra <gauravmishra2401@gmail.com>
2023-12-22 17:26:49 +0530

    added button, cursor interaction and icon

--- cursor.js
+++ cursor.js
@@ -1,4 +1,6 @@
 const cursor = document.getElementById('cursor');
+const largeCursorBoxes = document.querySelectorAll('.largecursor');
+const cursorColorBoxes = document.querySelectorAll('.cursorcolor');
 
 window.addEventListener('mousemove', (e) => {
     cursor.style.top = e.pageY + 'px';
@@ -7,4 +9,22 @@ window.addEventListener('mousemove', (e) => {
 window.addEventListener('scroll', (e) => {
     cursor.style.top = e.pageY + 'px';
     cursor.style.left = e.pageX + 'px';
+})
+
+largeCursorBoxes.forEach(box => {
+    box.addEventListener('mouseenter', (e) => {
+        cursor.style.width = "3rem";
+    })
+    box.addEventListener('mouseleave', (e) => {
+        cursor.style.width = "1rem";
+    })
+})
+
+cursorColorBoxes.forEach(box => {
+    box.addEventListener('mouseenter', (e) => {
+        cursor.style.borderColor = "#968200";
+    })
+    box.addEventListener('mouseleave', (e) => {
+        cursor.style.borderColor = "#FFE74C";
+    })
 })
