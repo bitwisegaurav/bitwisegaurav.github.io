7da0c29c4c1d3efd5b07fa67efd3c218d3f20a72
Gaurav Mishra <gauravmishra2401@gmail.com>
2023-12-24 04:55:15 +0530

    separated the intro part from header.js

--- js/header.js
+++ js/header.js
@@ -1,9 +1,4 @@
 const header = document.querySelector('header');
-let nameturn = false; // false means username and true means real name
-let text = nameturn ? "Gaurav Mishra" : "BitwiseGaurav";
-let index = 0;
-let timeouts = null;
-const userBox = document.querySelector('user');
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > window.innerHeight) {
@@ -12,13 +7,3 @@ window.addEventListener("scroll", () => {
         header.style.backgroundColor = "transparent";
     }
 })
-
-function changeName() {
-    let name = nameturn ? "Gaurav Mishra" : "BitwiseGaurav";
-    let username = nameturn ? "" : "User";
-    document.getElementById('introName').textContent = name;
-    userBox.textContent = username;
-    nameturn = !nameturn;
-    timeouts = setTimeout(changeName, 6000);
-}
-changeName();
