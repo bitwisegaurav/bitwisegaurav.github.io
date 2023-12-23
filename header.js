239bb900f50c4d747b0a3bffc2e3568f83dcf0e4
Gaurav Mishra <gauravmishra2401@gmail.com>
2023-12-24 04:27:42 +0530

    fixed locomotive issue and username issue

--- header.js
+++ header.js
@@ -3,6 +3,7 @@ let nameturn = false; // false means username and true means real name
 let text = nameturn ? "Gaurav Mishra" : "BitwiseGaurav";
 let index = 0;
 let timeouts = null;
+const userBox = document.querySelector('user');
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > window.innerHeight) {
@@ -14,7 +15,9 @@ window.addEventListener("scroll", () => {
 
 function changeName() {
     let name = nameturn ? "Gaurav Mishra" : "BitwiseGaurav";
+    let username = nameturn ? "" : "User";
     document.getElementById('introName').textContent = name;
+    userBox.textContent = username;
     nameturn = !nameturn;
     timeouts = setTimeout(changeName, 6000);
 }
