fc635dc6700b58985a80e23ecbe1ce11ec1cd824
Gaurav Mishra <gauravmishra2401@gmail.com>
2024-01-12 11:15:43 +0530

    fixed the active button issue for about section

--- js/active.js
+++ js/active.js
@@ -1,23 +1,27 @@
-window.addEventListener('scroll', function () {
-    let sections = document.querySelectorAll('section');
-    let currentSection = null;
-  
-    sections.forEach(function (section) {
-      let rect = section.getBoundingClientRect();
-      if (rect.top >= 0 && rect.bottom <= (window.innerHeight + 150)) {
-        currentSection = section;
-      }
-    });
-  
-    if (currentSection !== null) {
-      let navItems = document.querySelectorAll('.active');
-      navItems.forEach(function (navItem) {
-        navItem.classList.remove('active');
-      });
-  
-      let currentNavItem = document.querySelector('[href="#' + currentSection.id + '"]');
-      if (currentNavItem !== null) {
-        currentNavItem.classList.add('active');
-      }
+window.addEventListener("scroll", function () {
+  let sections = document.querySelectorAll("section");
+  let currentSection = null;
+
+  // console.log(sections);
+
+  sections.forEach(function (section) {
+    let rect = section.getBoundingClientRect();
+    if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+      currentSection = section;
     }
   });
+
+  if (currentSection !== null) {
+    let navItems = document.querySelectorAll(".active");
+    navItems.forEach(function (navItem) {
+      navItem.classList.remove("active");
+    });
+
+    let currentNavItem = document.querySelector(
+      '[href="#' + currentSection.id + '"]'
+    );
+    if (currentNavItem !== null) {
+      currentNavItem.classList.add("active");
+    }
+  }
+});
